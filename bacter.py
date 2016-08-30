810f84ed3ba8d8da10a9cadf951b4deb26995e49
Dylan Rees <dylanrees@gmail.com>
2016-08-30 19:44:03 -0400

    program now successfully performs a linear curve fit

--- bacter.py
+++ bacter.py
@@ -36,9 +36,22 @@ def f(x, m, b):
     y = m*x + b
     return y
 
+# this might help: http://www2.mpia-hd.mpg.de/~robitaille/PY4SCI_SS_2014/_static/15.%20Fitting%20models%20to%20data.html
 popt, pcov = curve_fit(f, x, y)
-print popt[0]
-print popt[1]
+print("Here are your curve fit values:")
+print(popt[0])
+print(popt[1])
+
+#calculate data length.  this will be truncated if some of your values aren't good
+print("Here are the endpoint values of x:")
+print((min(x)))
+x1 = min(x)
+print(f(min(x),popt[0],popt[1]))
+y1 = f(min(x),popt[0],popt[1])
+print((max(x)))
+x2 = max(x)
+print(f(max(x),popt[0],popt[1]))
+y2 = f(max(x),popt[0],popt[1])
 
 #initialize plot and plot for the first time
 
@@ -46,11 +59,13 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
 # plot data
-ax1.scatter(x,y,color='blue',s=5,edgecolor='none')
-ax1.set_aspect(1./ax1.get_data_ratio()) # make axes square
+
+#ax1.scatter(x,y,color='blue',s=5,edgecolor='none')
 
 plt.plot(x,y, color="blue")
-plt.plot(x, f(x,popt[0],popt[1]), color="red")
+ax1.set_aspect(1./ax1.get_data_ratio()) # make axes square
+#plt.plot(x, f(x,popt[0],popt[1]), color="red")
+plt.plot([x1,x2],[y1,y2],marker="o",color="red")
 
 # show the plot
 plt.show()
